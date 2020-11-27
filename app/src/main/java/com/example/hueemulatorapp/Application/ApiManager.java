be7c120c7acfb7b928d3428ac2e6ad3d25d1e3a5
Redouan Khanfous <r.khanfous@student.avans.nl>
2020-11-27 15:39:13 +0100

    Added some stuff for the api manager class

---
+++ app/src/main/java/com/example/hueemulatorapp/Application/ApiManager.java
@@ -0,0 +1,10 @@
+package com.example.hueemulatorapp.Application;
+
+import com.android.volley.RequestQueue;
+
+public class ApiManager {
+    private static final String LOGTAG = ApiManager.class.getName();
+
+    private RequestQueue requestQueue;
+
+}
