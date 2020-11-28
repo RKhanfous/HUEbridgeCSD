ca364b47f803f5c5e4bd1828dbbe6bf4b82cfea7
Logophilist <Ralf@van-aert.nl>
2020-11-28 14:49:14 +0100

    [ADD] HttpClient

---
+++ app/src/main/java/com/example/hueemulatorapp/Application/HttpClient.java
@@ -0,0 +1,45 @@
+package com.example.hueemulatorapp.Application;
+
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class HttpClient {
+
+    private static final String TAG = OkHttpClient.class.getSimpleName();
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    private OkHttpClient httpClient = new OkHttpClient();
+
+    public Response get(String url) throws IOException {
+        Log.i(TAG, "Sending a get request with URL: " + url);
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+        return httpClient.newCall(request).execute();
+    }
+
+    public Response put(String url, String json) throws IOException {
+        Log.i(TAG, "Sending a put request with body:\n" + json + "\n to URL: " + url);
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .put(body)
+                .build();
+        return httpClient.newCall(request).execute();
+    }
+
+    public void setCallback(Request request, Callback callback){
+        httpClient.newCall(request).enqueue(callback);
+        JSONObject jsonObject = new JSONObject();
+    }
+}
