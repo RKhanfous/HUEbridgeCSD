305a1fdca21225d04c871b2f631bfc3789915a6d
Logophilist <Ralf@van-aert.nl>
2020-12-01 12:07:29 +0100

    JsonData refactor

--- app/src/main/java/com/example/hueemulatorapp/Application/JsonData.java
+++ app/src/main/java/com/example/hueemulatorapp/Application/JsonData.java
@@ -1,43 +1,89 @@
 package com.example.hueemulatorapp.Application;
 
+import android.util.JsonReader;
+import android.util.Log;
+
+import com.example.hueemulatorapp.Data.Lamp;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONStringer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonData {
 
-    public static final String lights = "/api/logophilist/lights";
+    public static final String TAG = JsonData.class.getName();
+
+    public static final String lights = "/api/newdeveloper/lights";
     public static final String setState = "/state";
+    public static final String uri = "http://192.168.178.192:8000";
 
 
-    public static String putJsonLightsRename(String name){
-        JSONObject postdata = new JSONObject();
+    public static String getBodyRename(String name){
+        JSONObject body = new JSONObject();
         try{
-            postdata.put("name", name);
+            body.put("name", name);
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        return postdata.toString();
+        return body.toString();
     }
 
-    public static String putJsonLightsOnOff(Boolean OnOff){
-        JSONObject postdata = new JSONObject();
+    public static String getBodyLightOn(Boolean lightOn){
+        JSONObject body = new JSONObject();
         try{
-            postdata.put("on", OnOff);
+            body.put("on", lightOn);
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        return postdata.toString();
+        return body.toString();
     }
 
-    public static String putJsonLightsHueBriSat(int hue, int bri, int sat){
-        JSONObject postdata = new JSONObject();
+    public static String getBodyColor(int hue, int bri, int sat){
+        JSONObject body = new JSONObject();
         try{
-            postdata.put("hue", hue);
-            postdata.put("bri", bri);
-            postdata.put("sat", sat);
+            body.put("hue", hue);
+            body.put("bri", bri);
+            body.put("sat", sat);
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        return postdata.toString();
+        return body.toString();
+    }
+
+    public static ArrayList<Lamp> readGetLightsResponse(String jsonResponse) throws JSONException {
+        JSONObject response = new JSONObject(jsonResponse);
+        ArrayList<Lamp> lamps = new ArrayList<>();
+        boolean done = false; int indexLamp = 1; Lamp addingLamp; JSONObject addingJson;
+        while (!done){
+            try{
+                addingJson = response.getJSONObject(String.valueOf(indexLamp));
+                JSONObject state = addingJson.getJSONObject("state");
+                JSONObject config = addingJson.getJSONObject("config");
+
+                addingLamp = new Lamp(
+                        config.getString("uniqueid"), //Get unique ID
+                        addingJson.getString("type"), //Get type.
+                        addingJson.getString("name"), //Get Name
+                        addingJson.getString("modelid"), //Get model ID
+                        addingJson.getString("productname"),
+                        state.getBoolean("on"), // Get lights on
+                        state.getInt("bri"), // Get brightness
+                        state.getInt("hue"), // Get Hue
+                        state.getInt("sat"), // Get saturation
+                        state.getString("effect") // Get effect
+                );
+
+                lamps.add(addingLamp);
+
+            } catch (JSONException e){
+                done = true;
+                Log.d(TAG, "Stopped finding lights at: " + indexLamp);
+            }
+        }
+
+        return lamps;
     }
 }
