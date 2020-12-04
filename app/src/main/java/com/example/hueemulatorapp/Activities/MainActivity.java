394bcd05d82955bbc12de7fb42208e9077552dcc
Logophilist <Ralf@van-aert.nl>
2020-12-04 15:33:03 +0100

    port menu dialog
    todo sharedpreferences

--- app/src/main/java/com/example/hueemulatorapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/hueemulatorapp/Activities/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.hueemulatorapp.Activities;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.example.hueemulatorapp.Data.DimLight;
+import com.example.hueemulatorapp.Data.HttpParser;
 import com.example.hueemulatorapp.Data.HueLight;
 import com.example.hueemulatorapp.R;
 
@@ -22,6 +29,15 @@ public class MainActivity extends AppCompatActivity implements DetailFragmentRep
 
         FrameLayout mainframe = findViewById(R.id.main_framelayout);
         mainframe.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+
+        ImageButton buttonPort = findViewById(R.id.button_portmenu);
+        buttonPort.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                showPortDialog();
+            }
+        });
 
         Fragment mainFragment = new MainFragment(this, this);
 
@@ -44,5 +60,31 @@ public class MainActivity extends AppCompatActivity implements DetailFragmentRep
                 .replace(R.id.main_framelayout, new DetailFragmentDim(light), DetailFragmentDim.TAG)
                 .addToBackStack(DetailFragmentDim.TAG)
                 .commit();
+    }
+
+    public void showPortDialog(){
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.port_menu,  null);
+        dialogBuilder.setView(dialogView);
+        final EditText editPort = dialogView.findViewById(R.id.editText_port);
+
+        dialogBuilder.setTitle(getResources().getString(R.string.app_name));
+        dialogBuilder.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                HttpParser.getInstance().setPort(Integer.parseInt(editPort.getText().toString()));
+                Toast.makeText(MainActivity.this, "Port changed to " + editPort.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        dialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Canceled changing ports", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        AlertDialog dialog = dialogBuilder.create();
+        dialog.show();
     }
 }
